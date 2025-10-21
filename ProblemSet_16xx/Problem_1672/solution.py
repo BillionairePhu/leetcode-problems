dc9c5a26850fe36d7d2e0fec421f5ce67dd21ba6
BillionaireP <96333266+phuhuynhlean@users.noreply.github.com>
2025-10-21 21:59:10 +0700

    Problem 1672

---
+++ ProblemSet_16xx/Problem_1672/solution.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+  def maximumWealth(self, accounts: List[List[int]]) -> int:
+    result = 0
+    for account in accounts:
+      result = max(result, sum(account))
+    return result
